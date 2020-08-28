7baa53ca8f32f4e4d5d024b38eb97f8982b43dca
Gary Chang <gary.chang.dev@gmail.com>
2020-08-28 06:03:39 -0400

    Initial commit

---
+++ Assets/Scripts/Common/PlayerInputHandler.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public class BoolUnityEvent : UnityEvent<bool> { }
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public UnityEvent onJumpEvent;
+    public UnityEvent onAttackEvent;
+    public BoolUnityEvent onBlockEvent;
+
+    private Vector2 m_MoveDirection;
+    private Vector2 m_LookDirection;
+    
+    private PlayerInputActions m_PlayerInputActions;
+
+    void Awake()
+    {
+        m_PlayerInputActions = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        m_PlayerInputActions.Enable();
+        m_PlayerInputActions.Player.Jump.performed += OnJump;
+        m_PlayerInputActions.Player.Attack.performed += OnAttack;
+
+        m_PlayerInputActions.Player.Block.started += OnBlockStarted;
+        m_PlayerInputActions.Player.Block.canceled += OnBlockCanceled;
+    }
+
+    void OnDisable()
+    {
+        m_PlayerInputActions.Disable();
+        m_PlayerInputActions.Player.Jump.performed -= OnJump;
+        m_PlayerInputActions.Player.Attack.performed -= OnAttack;
+
+        m_PlayerInputActions.Player.Block.started -= OnBlockStarted;
+        m_PlayerInputActions.Player.Block.canceled -= OnBlockCanceled;
+    }
+
+    private void Update()
+    {
+        m_MoveDirection = m_PlayerInputActions.Player.Move.ReadValue<Vector2>();
+        m_LookDirection = m_PlayerInputActions.Player.Look.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        onJumpEvent?.Invoke();
+    }
+
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        onAttackEvent?.Invoke();
+    }
+
+    public void OnBlockStarted(InputAction.CallbackContext ctx)
+    {
+        onBlockEvent?.Invoke(true);
+    }
+
+    public void OnBlockCanceled(InputAction.CallbackContext ctx)
+    {
+        onBlockEvent?.Invoke(false);
+    }
+
+    public Vector2 GetMoveDirection()
+    {
+        return m_MoveDirection;
+    }
+
+    public Vector2 GetLookDirection()
+    {
+        return m_LookDirection;
+    }
+}
---
+++ Assets/Scripts/Scene3/PlayerControllerV2.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class PlayerControllerV2 : MonoBehaviour
+{
+    private Animator m_Animator;
+    private PlayerInputHandler m_PlayerInputHandler;
+
+    // For smooth movement purposes
+    public float smoothingFactor = 5f;
+    private Vector3 m_CurrentDirection;
+
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_PlayerInputHandler = GetComponent<PlayerInputHandler>();
+    }
+
+    private void OnEnable()
+    {
+        m_PlayerInputHandler.onBlockEvent.AddListener(OnBlock);
+        m_PlayerInputHandler.onAttackEvent.AddListener(OnAttack);
+    }
+
+    private void OnDisable()
+    {
+        m_PlayerInputHandler.onBlockEvent.RemoveListener(OnBlock);
+        m_PlayerInputHandler.onAttackEvent.RemoveListener(OnAttack);
+    }
+
+    private void Update()
+    {
+        UpdatePosition();
+        UpdateRotation();
+    }
+
+    private Vector3 ConvertToPlayerDirection(Vector2 moveDirection)
+    {
+        Vector3 direction = new Vector3(moveDirection.x, 0f, moveDirection.y);
+        direction = Vector3.ClampMagnitude(direction, 1f);
+        return direction;
+    }
+
+    private void UpdatePosition()
+    {
+        Vector2 moveDirection = m_PlayerInputHandler.GetMoveDirection();
+        Vector3 direction = ConvertToPlayerDirection(moveDirection);
+
+        // Smooth out the direction changes
+        Vector3 change = direction - m_CurrentDirection;
+        m_CurrentDirection += change * smoothingFactor * Time.deltaTime;
+
+        // TODO: Set blending parans for animator
+    }
+
+    private void UpdateRotation()
+    {
+        Vector2 lookRotation = m_PlayerInputHandler.GetLookDirection();
+        m_Animator.SetFloat("turn", lookRotation.x);
+    }
+
+    private void OnAttack()
+    {
+        // TODO: Trigger the attack animation
+    }
+
+    private void OnBlock(bool blockStatus)
+    {
+        // TODO: Trigger the block animation
+    }
+}
